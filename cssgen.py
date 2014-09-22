29a306cfc77252c71d8bb3b2ab44a31aea106c03
vml-akoebbe <akoebbe@vml.com>
2014-09-22 13:51:04 -0500

    Fixed formating and typos

--- cssgen.py
+++ cssgen.py
@@ -1,4 +1,13 @@
-itteration = 0
-while itteration < 4000:
-  print ".class-" + str(itteration) + "{ color: blue; font-weight: bold; background: yellow; font-family: sans-serif; border-size: 2px; border-color: blue; margin-left: 1rem; margin-right: 1rem; margin-top; 1rem; margin-bottom: 1rem}"
-  itteration += 1
+iteration = 0
+while iteration < 4000:
+    print ".class-" + str(iteration) + "{ color: blue;" \
+        "font-weight: bold;" \
+        "background: yellow;" \
+        "font-family: sans-serif;" \
+        "border-size: 2px;" \
+        "border-color: blue;" \
+        "margin-left: 1rem;" \
+        "margin-right: 1rem;" \
+        "margin-top; 1rem;" \
+        "margin-bottom: 1rem }"
+    iteration += 1
